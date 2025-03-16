82019deeceb49962a278111cd1e96d9e881b57d7
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-16 21:07:27 +0300

    fix the end point link of the api

--- src/main.js
+++ src/main.js
@@ -7,24 +7,20 @@ const imageInput = document.querySelector(`.image-input`);
 const factButton = document.querySelector(`#submit-fact`);
 const photoButton = document.querySelector(`#submit-photo`);
 const displayImage = document.querySelector(`.display-image`);
-let errorMessage = `Failed,try again later`;
+let errorMessage = `Failed, try again later`;
 
 async function getCatFacts() {
-  displayFacts.innerHTML="";
-  displayImage.innerHTML = ``;
+  displayFacts.innerHTML = "";
+  displayImage.innerHTML = "";
   try {
-    let catFacts = factInput.value || 1;
+    let catFacts = parseInt(factInput.value) || 1;
 
-    if (catFacts > 50) {
-      catFacts = 50;
-    }
-    const response = await fetch(`http://meowfacts.herokuapp.com/?count=${catFacts}`);
+    // Limit input between 1 and 50
+    if (catFacts > 50) catFacts = 50;
+    if (catFacts < 1) catFacts = 1;
+
+    const response = await fetch(`https://meowfacts.herokuapp.com/?count=${catFacts}`);
     const data = await response.json();
-    console.log(data.data);
-
-    // if (!response.ok) {
-    //   throw new Error(`Failed, Please try again later.`);
-    // }
 
     let facts = Array.isArray(data.data) ? data.data : [data.data];
     const factList = document.createElement(`ol`);
@@ -41,40 +37,8 @@ async function getCatFacts() {
     displayFacts.appendChild(factList);
   } catch (error) {
     errorDisplay.classList.add(`error-box`);
-    errorDisplay.innerHTML=`<p class="error-message">${errorMessage}</P>`;
+    errorDisplay.innerHTML = `<p class="error-message">${errorMessage}</P>`;
   }
 }
 
-
-async function getCatsImages() {
-  displayFacts.innerHTML = ``;
-  displayImage.innerHTML = ``;
-  try {
-    let catImage = imageInput.value || 1;
-    if(catImage > 10) {
-      catImage = 10;
-    }
-    const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=${catImage}`);
-    const data=await response.json();
-    data.forEach((cat) => {
-      const img=document.createElement(`img`);
-      img.src=cat.url;
-      img.alt=`A Cat`;
-      img.style.width= "19rem";
-      img.style.margin= `0.5rem`;
-      displayImage.appendChild(img);
-    })
-  } catch (error) {
-    errorDisplay.classList.add(`error-box`);
-    errorDisplay.innerHTML=`<p class="error-message">${errorMessage}</P>`;
-        
-  }
-}
-
-
-
-factButton.addEventListener(`click`, getCatFacts);
-photoButton.addEventListener(`click`, getCatsImages);
-
-
-
+factButton.addEventListener("click", getCatFacts);
